b4850f41fa6f590ac991bc4b8c14ed4fe850c021
涛旭 邹 <1595206644@qq.com>
2020-03-28 23:07:09 +0800

    .

---
+++ C Primer Plus/第一个c程序/源.cpp
@@ -0,0 +1,11 @@
+/*这是C Primer Plus学习的第一个c程序*/
+#include<stdio.h>
+int main(void)
+{
+	int num;
+	num = 1;
+	printf_s("I am a simple");
+	printf_s("computer.\n");
+	printf_s("My favorite number is %d because it is first.\n", num);
+	return 0;
+}

2e2ffa8efda742602aa1d6905a12ec3b536c6fe6
涛旭 邹 <1595206644@qq.com>
2020-04-20 00:25:24 +0800

    Revert "4.20"
    This reverts commit e3c5df2dce408b5a8fbec90929bd5abf78b7baea.

--- C Primer Plus/4.3（praise2）/源.cpp
+++ C Primer Plus/4.3（praise2）/源.cpp
@@ -4,12 +4,12 @@
 int main(void)
 {
 	char name[40];
-	printf_s("What's your name?");//你的名字是？
+	printf_s("What's your name?");
 	scanf_s("%s", &name, sizeof(name));
-	printf_s("Hello,%s.%s \n", name, PRAISE);//你好，name，你是一个非凡的存在
+	printf_s("Hello,%s.%s \n", name, PRAISE);
 	printf_s("Your name of %zd letters occupies %zd memory cells.\n",
-		strlen(name), sizeof (name));//您的%zd个字母的名称占用了%zd个内存单元。
+		strlen(name), sizeof (name));
 	printf_s("The phrase of praise has %zd letters",
-		strlen(PRAISE));//这句赞美的话有%zd个字母
+		strlen(PRAISE));
 	return 0;
 }
